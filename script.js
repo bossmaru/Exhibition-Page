7002428fcf7781d984b102d1b878f79e411541bb
bossmaru <rucy951@gmail.com>
2024-09-04 23:30:53 +0900

    add list test

--- script.js
+++ script.js
@@ -4,34 +4,53 @@ var mapContainer = document.getElementById('map'),
         level: 7 // 기본 확대 레벨
     };
 
-// 모바일 화면 크기 조정
-if (window.innerWidth < 768) {
-    mapOption.level = 9; // 모바일에서는 더 넓게 보기 위해 확대 레벨 변경
-}
-
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도 생성
 
-// 전시회 위치 (마커로 표시)
-var positions = [
+var marker = new kakao.maps.Marker({
+    map: map,
+    position: new kakao.maps.LatLng(0, 0), // 초기 위치를 0으로 설정, 마우스 오버 시 위치 업데이트
+    visible: false // 마커는 기본적으로 숨김
+});
+
+// 전시회 및 관광지 정보 배열
+var exhibitions = [
     {
-        title: '서울 전시회 1', 
-        latlng: new kakao.maps.LatLng(37.5665, 126.9780)
+        title: '경복궁',
+        latlng: new kakao.maps.LatLng(37.5796, 126.9770),
+        description: '서울에서 가장 유명한 궁궐입니다.',
+        link: 'https://www.royalpalace.go.kr/'
     },
     {
-        title: '서울 전시회 2', 
-        latlng: new kakao.maps.LatLng(37.5796, 126.9769)
+        title: '국립중앙박물관',
+        latlng: new kakao.maps.LatLng(37.5231, 126.9803),
+        description: '한국의 역사와 문화를 보여주는 박물관입니다.',
+        link: 'https://www.museum.go.kr/'
     },
     {
-        title: '부산 전시회 3', 
-        latlng: new kakao.maps.LatLng(35.1796, 129.0756)
+        title: '남산 서울타워',
+        latlng: new kakao.maps.LatLng(37.5512, 126.9882),
+        description: '서울의 아름다운 전경을 감상할 수 있는 타워입니다.',
+        link: 'https://www.nseoultower.co.kr/'
     }
 ];
 
-// 마커 생성 및 지도에 추가
-for (var i = 0; i < positions.length; i++) {
-    var marker = new kakao.maps.Marker({
-        map: map, // 마커를 표시할 지도 객체
-        position: positions[i].latlng, // 마커의 위치
-        title: positions[i].title // 마커 타이틀
+// 전시회 리스트를 HTML로 생성
+var exhibitionList = document.getElementById('exhibitionList');
+exhibitions.forEach(function(exhibit, index) {
+    var listItem = document.createElement('li');
+    listItem.innerHTML = `<strong>${exhibit.title}</strong><br>${exhibit.description}<br><a href="${exhibit.link}" target="_blank">자세히 보기</a>`;
+    
+    // 마우스 오버 시 마커 표시
+    listItem.addEventListener('mouseover', function() {
+        marker.setPosition(exhibit.latlng); // 마커 위치 업데이트
+        marker.setVisible(true); // 마커 보이기
+        map.panTo(exhibit.latlng); // 지도를 마커 위치로 이동
     });
-}
+
+    // 마우스가 리스트를 벗어나면 마커 숨기기
+    listItem.addEventListener('mouseout', function() {
+        marker.setVisible(false);
+    });
+
+    exhibitionList.appendChild(listItem);
+});
